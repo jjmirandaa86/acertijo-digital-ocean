c75161054a3cbad7141da6c2cdb0606de4908190
jjmirandaa86 <jjmirandaa86@gmail.com>
2026-03-16 20:19:38 +1100

    Setup services

---
+++ README.md
@@ -0,0 +1,13 @@
+## PRODUCTION
+
+chmod +x start-services-prod.sh
+./start-services-prod.sh
+
+## DEVELOPER
+
+chmod +x start-services-dev.sh
+./start-services-dev.sh
+
+## If I wanna up 1 services
+
+docker compose --env-file .env.dev -f node-compose.yml up -d
---
+++ start-services-prod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Mostrar cada comando antes de ejecutarlo
+set -x
+
+# Levantar todos los servicios
+docker compose \
+  --env-file .env.prod \
+  -f mysql-compose.yml \
+  -f mongodb-compose.yml \
+  -f node-compose.yml \
+  -f nginx-compose.yml \
+  up -d
